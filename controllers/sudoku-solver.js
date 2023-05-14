d52e3de1e7ba205f45bbd0220fce53d9cb7a9ce1
Pavel Zubko <tyyrok@gmail.com>
2023-05-14 19:54:46 +0000

    feat: validate, check made

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,23 +1,136 @@
 class SudokuSolver {
-
+  constructor(){
+    this.grid = { a: 0, b: 9, c: 18, d: 27, e: 36, f: 45, g: 54, h: 63, i: 72 };
+    this.additionalGrid = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
+  }
+  
   validate(puzzleString) {
+    
+    //{ error: 'Expected puzzle to be 81 characters long' }
+    if (puzzleString.length != 81) {
+      return { error: 'Expected puzzle to be 81 characters long' };
+    } else {
+      //{ error: 'Invalid characters in puzzle' }
+      if (!puzzleString.match(/^[1-9.]+$/g)){
+        return { error: 'Invalid characters in puzzle' };
+      } else {
+        return { result: 'correct' }
+      }
+    }
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
 
+    for (let key in this.grid){
+      if (key == row.toLowerCase()) {
+        console.log("Row For Check - " + puzzleString.slice(this.grid[key], this.grid[key] + 9));
+        console.log("Value - " + value);
+        if (puzzleString.slice(this.grid[key], this.grid[key] + 9).includes(value)) {
+          return false;
+        }
+        break;
+      }
+    }
+    console.log("Row ok!")
+    return true;
   }
 
   checkColPlacement(puzzleString, row, column, value) {
+    
+    let rowForCheck = '';
 
+    for (let i = 0; i < 9; i++) {
+        rowForCheck += puzzleString[ (column - 1) + (i * 9) ];
+    }
+    
+    console.log("Col For Check - " + rowForCheck);
+    if (rowForCheck.includes(value)){
+      return false;
+    }
+    console.log("Col ok!");
+    return true;
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
+    
+    let regionForCheck = '';
+    let startPointRegion = this.grid[row];
+    console.log(this.grid[row]);
+    console.log("Start Point for region " + startPointRegion);
+    
+    switch (startPointRegion % 27 ) {
+      case 9:
+        startPointRegion -= 9; 
+        break;
+      case 18:
+        startPointRegion -= 18;
+        break;
+    }
+    if (column % 9 == 0) column -= 3;
+    startPointRegion += (column - ( column % 3) ); 
+    console.log("StartPoint Region - " + startPointRegion);
 
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0 ; j < 3; j++) {
+        regionForCheck += puzzleString[startPointRegion + j];
+      }
+      startPointRegion += 9;
+    }
+    
+    console.log("Region For Check - " + regionForCheck);
+    if (regionForCheck.includes(value)){
+      return false;
+    }
+    console.log("Region ok!")
+    return true;
+    
   }
 
   solve(puzzleString) {
     
-  }
+    let switcher = false;
+    
+    while (puzzleString.includes('.')){
+      
+      let indexOfDot = puzzleString.indexOf('.');
+      switcher = false;
+      for (let j = 1; j <= 9; j ++){
+
+        let row = this.additionalGrid[(indexOfDot - (indexOfDot % 9))/9];
+        let value = j;
+        
+        let column = (indexOfDot % 9 == 0)? 1 : (indexOfDot % 9) + 1;
+
+        console.log("--------------");
+        console.log("Column - " + column);
+        console.log("Row - " + row);
+        console.log("Value - " + value);
+        
+        if (this.checkRowPlacement(puzzleString, row, column, value)){
+          if (this.checkColPlacement(puzzleString, row, column, value)){
+            if (this.checkRegionPlacement(puzzleString, row, column, value)){
+              let newPuzzleString = puzzleString.replace('.', value);
+              
+              console.log("New puzzle string - " + newPuzzleString);
+              
+              puzzleString = newPuzzleString.slice();
+              console.log("New string - " + puzzleString);
+              switcher = true;
+            } 
+          }
+        }
+        if (switcher) break;
+      }
+      if (!switcher) {
+        console.log("Houston we have a problem!");
+        break;
+      } 
+    }
+    
+    console.log("Final Puzzle String - " + puzzleString);
+    
+    return puzzleString;
+  } 
 }
 
 module.exports = SudokuSolver;
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,67 @@
 const chai = require('chai');
 const assert = chai.assert;
-
 const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+let solver = new Solver();
+
+import {puzzlesAndSolutions} from '../controllers/puzzle-strings.js';
+const stringWithInvalidChar = '1.5..2.84..63.12.7.|..5.....9..1....8.2.3674.3.7.2..9D47...8..1..16....926914.37.';
+const stringMoreThan81 = '1.5..2.84..63.12.7.9..5.....9..9....8.2.3674.3.7.2..9947...8..9..16....9269314.37.';
+const defaultPuzzle = "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..";
+
 
 suite('Unit Tests', () => {
+  /*
+  test('#1 Logic handles a valid puzzle string of 81 characters', (done) => {
+    for (let prop of puzzlesAndSolutions){
+      assert.property(solver.validate(prop[0]), 'result');
+    }
+    done();
+  });
+
+  test('#2 Logic handles a puzzle string with invalid characters (not 1-9 or .)', (done) => {
+    assert.include(solver.validate(stringWithInvalidChar), { error: 'Invalid characters in puzzle' });
+    done();
+  });
+
+  test('#3 Logic handles a puzzle string that is not 81 characters in length', (done) => {
+    assert.include(solver.validate(stringMoreThan81), { error: 'Expected puzzle to be 81 characters long' });
+    done();
+  });
+
+  test('#4 Logic handles a valid row placement', (done) => {
+    assert.equal(solver.checkRowPlacement(defaultPuzzle, 'A', 1, 7), true);
+    done();
+  });
+
+  test('#5 Logic handles an invalid row placement', (done) => {
+    assert.equal(solver.checkRowPlacement(defaultPuzzle, 'A', 1, 9), false);
+    done();
+  });
+
+  test('#6 Logic handles a valid column placement', (done) => {
+    assert.equal(solver.checkColPlacement(defaultPuzzle, 'A', 7, 3), true);
+    done();
+  });
+
+  test('#7 Logic handles an invalid column placement', (done) => {
+    assert.equal(solver.checkColPlacement(defaultPuzzle, 'A', 7, 6), false);
+    done();
+  });
+
+  test('#8 Logic handles a valid region (3x3 grid) placement', (done) => {
+    assert.equal(solver.checkRegionPlacement(defaultPuzzle, 'd', 4, 8), true);
+    done();
+  });
+
+  test('#9 Logic handles an invalid region (3x3 grid) placement', (done) => {
+    assert.equal(solver.checkRegionPlacement(defaultPuzzle, 'h', 7, 4), false);
+    done();
+  });
+  
+  */
+  test('#10 Valid puzzle strings pass the solver', (done) => {
+    assert.isString(solver.solve(defaultPuzzle));
+    done();
+  });
 
 });
